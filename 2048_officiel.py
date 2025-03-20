7ff77d6fb50075826ec9551d69393e096374a1d4
ichraf01 <ichrafdarghouth1818@gmail.com>
2025-03-20 18:18:17 +0100

    le canvas de 2048

--- 2048_officiel.py
+++ 2048_officiel.py
@@ -1,3 +1,25 @@
-canvas=0
+import tkinter as tk
+import random
 
-canvas.square()
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+
+def create_2048_canvas():
+    root = tk.Tk()
+    root.title("2048 Canvas")
+    
+    canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white")
+    canvas.pack()
+
+    x0 = 100
+    x1 = CANVAS_WIDTH - 100
+    y = CANVAS_HEIGHT / 2
+    
+    # Ici c'est la ligne centrale
+    canvas.create_line(x0, y, x1, y, width=2, fill="black")
+    
+    # Les filles la j'ai fait les cercles
+    canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50, outline="black", width=2)
+    canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50, outline="black", width=2)
+    canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50, outline="black", width=2)
+
