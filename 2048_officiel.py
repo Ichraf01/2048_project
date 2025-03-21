3e01ab05076a62a29e32ea6b567a1331551930e5
ichraf01 <ichrafdarghouth1818@gmail.com>
2025-03-21 10:17:25 +0100

    jai trouvé

--- 2048_officiel.py
+++ 2048_officiel.py
@@ -17,3 +17,8 @@ def create_2048_canvas():
     
     # Ici c'est la ligne centrale
     canvas.create_line(x0, y, x1, y, width=2, fill="black")
+    
+    # Les filles la j'ai fait les rectangles j'ai pas aime les cercles
+    canvas.create_rectangle(x0 - 50, y + 50, x0 + 50, y - 50, outline="black", width=2)
+    canvas.create_rectangle(x1 - 50, y + 50, x1 + 50, y - 50, outline="black", width=2)
+    canvas.create_rectangle((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50, outline="black", width=2)
