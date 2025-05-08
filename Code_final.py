42618c38a9289a706afb91adb50a981f426a8d4e
ichraf01 <ichrafdarghouth1818@gmail.com>
2025-05-09 00:33:45 +0200

    hola

--- Code_final.py
+++ Code_final.py
@@ -13,9 +13,9 @@ couleurs = {
 
 
 def merge(ligne, taille_grille):
-    nv_ligne = [value for value in ligne if value != 0]  # liste contenant le val de chq cell sauf les 0
+    nv_ligne = [value for value in ligne if value != 0]  # liste  nt le val de chq cell sauf les 0
     i = 0  # compteur
-    while i < len(nv_ligne) - 1:  # condition d'arret: arreter quand i + grand que la taille de la list nv_ligne
+    while i < len(nv_ligne) - 1:  # condition d'arret: arreter quand i +contena grand que la taille de la list nv_ligne
         if nv_ligne[i] == nv_ligne[i + 1]:  # comparer si les elements de la liste 2 par 2 sont égaux
             nv_ligne[i] *= 2  # si oui on multiplie l'element i par 2
             nv_ligne[i + 1] = 0  # et l'element i+1 devient =0
@@ -75,9 +75,13 @@ for i in range(TAILLE):  # parcours des lignes
 
 
 def remp_alea():
+    # Crée une liste de toutes les positions vides (valeurs égales à 0) dans la grille
     empty = [(i, j) for i in range(TAILLE) for j in range(TAILLE) if grid_values[i][j] == 0]
+    # Vérifie s'il existe au moins une case vide
     if empty:
+        # Choisit aléatoirement une des positions vides
         i, j = random.choice(empty)
+        # Remplit cette case avec un 2 ou un 4, choisis aléatoirement
         grid_values[i][j] = random.choice([2, 4])
 
 
@@ -131,27 +135,34 @@ def can_move():
 
 
 def game_over():
+    # Désactive les touches fléchées pour empêcher tout autre mouvement
     root.unbind("<Left>")
     root.unbind("<Right>")
     root.unbind("<Up>")
     root.unbind("<Down>")
+    # Affiche un message "GAME OVER" au centre de la fenêtre
     game_over_label = tk.Label(root, text="GAME OVER", font=("Helvetica", 24, "bold"), bg="#BBADA0", fg="red")
-    game_over_label.place(relx=0.5, rely=0.5, anchor="center")  # Center the label
+    game_over_label.place(relx=0.5, rely=0.5, anchor="center")  # Centre le label dans la fenêtre
 
 def you_win():
+    # Désactive les touches fléchées lorsque le joueur gagne
     root.unbind("<Left>")
     root.unbind("<Right>")
     root.unbind("<Up>")
     root.unbind("<Down>")
+    # Affiche un message "YOU WIN" au centre de la fenêtre
     win_label = tk.Label(root, text="YOU WIN", font=("Helvetica", 24, "bold"), bg="#BBADA0", fg="green")
-    win_label.place(relx=0.5, rely=0.5, anchor="center")  # Center the label    
+    win_label.place(relx=0.5, rely=0.5, anchor="center")  # Centre le label dans la fenêtre
 
 
 
 # initialization du jeu
 # ichraf
+
+# Place deux tuiles aléatoires au début du jeu
 for _ in range(2):
     remp_alea()
+# Met à jour l'affichage de la grille (fonction définit par Nada)
 renouveler()
 
 # touria
